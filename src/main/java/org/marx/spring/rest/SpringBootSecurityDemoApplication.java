37d4ceacaaf999589e65459b6c9a517b59592b1d
ErikMarx <121640897+MarxErik@users.noreply.github.com>
2024-08-03 18:32:42 +0300

    little changer

--- src/main/java/org/marx/spring/rest/SpringBootSecurityDemoApplication.java
+++ src/main/java/org/marx/spring/rest/SpringBootSecurityDemoApplication.java
@@ -9,6 +9,7 @@ public class SpringBootSecurityDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
         System.out.println(1);
+
     }
 
 }
